fcf48518381ad7351152da595e1294dfb4d97938
home <1023115946@qq.com>
2018-11-27 22:51:53 +0800

    remove ranking

--- js/chart.js
+++ js/chart.js
@@ -2,97 +2,97 @@ var chartH = $('.tab-pane').css('height')
 var chartW = $('.tab-pane').css('width')
 
 
-var myChart = echarts.init(document.getElementById('ranking'));
+// var myChart = echarts.init(document.getElementById('ranking'));
 
 
 
-var option = {
-    //     title: {
-    //     // text: '数据来自网络',
-    //     // subtext: '',
+// var option = {
+//     //     title: {
+//     //     // text: '数据来自网络',
+//     //     // subtext: '',
 
 
-    // },
-    title: {
-        text: '2018年各产区排名',
-        textStyle: {
-            color: '#5dc2fe',
-            fontSize: '16px'
-        },
-        bottom: '0'
-    },
-    tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-            type: 'shadow'
-        },
+//     // },
+//     title: {
+//         text: '2018年各产区排名',
+//         textStyle: {
+//             color: '#5dc2fe',
+//             fontSize: '16px'
+//         },
+//         bottom: '0'
+//     },
+//     tooltip: {
+//         trigger: 'axis',
+//         axisPointer: {
+//             type: 'shadow'
+//         },
 
-    },
-    legend: {
-        //     orient: 'horizontal', // 'vertical'     
+//     },
+//     legend: {
+//         //     orient: 'horizontal', // 'vertical'     
 
-        //     x: 'right', // 'center' | 'left' | {number},     
+//         //     x: 'right', // 'center' | 'left' | {number},     
 
-        //     y: 'top', // 'center' | 'bottom' | {number}       
+//         //     y: 'top', // 'center' | 'bottom' | {number}       
 
-        // //   backgroundColor: '#fff',        borderColor: 'rgba(178,34,34,0.8)',    
+//         // //   backgroundColor: '#fff',        borderColor: 'rgba(178,34,34,0.8)',    
 
-        //     //  borderWidth: 4,     
+//         //     //  borderWidth: 4,     
 
-        //     margin: 20,    // [5, 10, 15, 20]     
-        //     data: ['2012年'],
-        //     textStyle: {
-        //         color: 'white'
-        //     },
+//         //     margin: 20,    // [5, 10, 15, 20]     
+//         //     data: ['2012年'],
+//         //     textStyle: {
+//         //         color: 'white'
+//         //     },
 
-    },
-    grid: {
-        left: '1%',
-        right: '4%',
-        bottom: '1%',
-        containLabel: true,
-        height: chartH,
-        width: chartW
-    },
-    xAxis: {
-        show: false,
-        type: 'value',
-        boundaryGap: [0, 0.01]
-    },
-    yAxis: {
-        type: 'category',
-        data: ['第一产区', '第二产区', '第三产区',],
-        axisLabel: {
-            textStyle: {
-                color: '#5dc2fe'
-            }
-        }
-    },
-    series: [
+//     },
+//     grid: {
+//         left: '1%',
+//         right: '4%',
+//         bottom: '1%',
+//         containLabel: true,
+//         height: chartH,
+//         width: chartW
+//     },
+//     xAxis: {
+//         show: false,
+//         type: 'value',
+//         boundaryGap: [0, 0.01]
+//     },
+//     yAxis: {
+//         type: 'category',
+//         data: ['第一产区', '第二产区', '第三产区',],
+//         axisLabel: {
+//             textStyle: {
+//                 color: '#5dc2fe'
+//             }
+//         }
+//     },
+//     series: [
 
-        {
-            name: '2018年',
-            type: 'bar',
-            barWidth: 10,
-            data: [15, 20, 28],
-            itemStyle: {
-                normal: {
-                    color: function (params) {
-                        var colorList = [
-                            '#5e7e54', '#e44f2f', '#81b6b2', '#eba422', '#5e7e54',
-                            '#e44f2f', '#81b6b2', '#eba422', '#5e7e54', '#e44f2f'
-                        ];
-                        return colorList[params.dataIndex]
-                    },
+//         {
+//             name: '2018年',
+//             type: 'bar',
+//             barWidth: 10,
+//             data: [15, 20, 28],
+//             itemStyle: {
+//                 normal: {
+//                     color: function (params) {
+//                         var colorList = [
+//                             '#5e7e54', '#e44f2f', '#81b6b2', '#eba422', '#5e7e54',
+//                             '#e44f2f', '#81b6b2', '#eba422', '#5e7e54', '#e44f2f'
+//                         ];
+//                         return colorList[params.dataIndex]
+//                     },
 
-                }
-            },
-        }
-    ]
-};
+//                 }
+//             },
+//         }
+//     ]
+// };
 
 // 使用刚指定的配置项和数据显示图表。
-myChart.setOption(option);
+// myChart.setOption(option);
 
 var outChart = echarts.init(document.getElementById('out'));
 option = {
