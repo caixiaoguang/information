dc6cc1d12690a3cb3d7f9418a6360b82eb2fa517
caiMac <caixiaoguang@caideMacBook-Pro.local>
2018-12-03 09:11:30 +0800

    add yuanqu

--- js/chart.js
+++ js/chart.js
@@ -1,5 +1,4 @@
-var chartH = $('.tab-pane').css('height')
-var chartW = $('.tab-pane').css('width')
+
 
 
 // var myChart = echarts.init(document.getElementById('ranking'));
@@ -94,6 +93,8 @@ var chartW = $('.tab-pane').css('width')
 // 使用刚指定的配置项和数据显示图表。
 // myChart.setOption(option);
 
+
+//出口比例饼状图
 var outChart = echarts.init(document.getElementById('out'));
 option = {
     title: {
@@ -102,6 +103,9 @@ option = {
         textStyle: {
             color: '#5dc2fe'
         },
+    },
+    grid: {
+
     },
     tooltip: {
         trigger: 'item',
@@ -138,33 +142,143 @@ option = {
         }
     ]
 };
-
 outChart.setOption(option);
 
-var outChart = echarts.init(document.getElementById('last-five'));
+//各类型产量饼状图
+var varirtyChartH = parseInt($('#variety-info .tab-content').css('height')) - 37;
+console.log(varirtyChartH);
+
+$('#variety-ranking').css('height', varirtyChartH)
+
+
+
+
+var varirtyChart = echarts.init(document.getElementById('variety-ranking'));
+option = {
+    title: {
+        text: '各类型产量比例',
+        x: 'center',
+        textStyle: {
+            color: '#5dc2fe'
+        },
+    },
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['类型一', '类型二', '类型三', '类型四',],
+        textStyle: {
+            color: '#5dc2fe'
+        }
+    },
+    series: [
+        {
+            name: '出口占比',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+                { value: 335, name: '类型一' },
+                { value: 310, name: '类型二' },
+                { value: 234, name: '类型三' },
+                { value: 335, name: '类型四' },
+            ],
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.6)'
+                }
+            }
+        }
+    ]
+};
+varirtyChart.setOption(option);
+
+
+
+//近五年产量
+var lastFiveChart = echarts.init(document.getElementById('last-five'));
+// option = {
+//     textStyle: {
+//         color: '#5dc2fe'
+//     },
+//     title: {
+//         text: '近五年销售量',
+//         textStyle: {
+//             color: '#5dc2fe'
+//         },
+//         x: 'center'
+//     },
+//     xAxis: {
+//         type: 'category',
+//         data: ['2013', '2014', '2015', '2016', '2017'],
+//         axisLine: {
+//             lineStyle: {
+//                 color: '#5dc2fe',
+//             }
+//         }
+
+//     },
+//     yAxis: {
+//         type: 'value',
+//         axisLine: {
+//             lineStyle: {
+//                 color: '#5dc2fe',
+//             }
+//         },
+//         splitLine: {
+//             lineStyle: {
+//                 color: '#5dc2fe'
+//             }
+//         }
+//     },
+//     series: [{
+//         data: [820, 932, 901, 934, 1290],
+//         type: 'line',
+//     }],
+
+// };
+
 option = {
     textStyle: {
         color: '#5dc2fe'
     },
-    grid:{
-        
-    },
     title: {
-        text: '近五年产量',
+        text: '近五年销售量',
         textStyle: {
             color: '#5dc2fe'
         },
         x: 'center'
     },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data: ['网络销售量', '实体销售量', '总销售量',],
+        textStyle: {
+            color: '#5dc2fe'
+        },
+        top: '28px'
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
     xAxis: {
         type: 'category',
-        data: ['2013', '2014', '2015', '2016', '2017'],
+        boundaryGap: false,
+        data: ['2013', '2014', '2015', '2016', '2017',],
         axisLine: {
             lineStyle: {
                 color: '#5dc2fe',
             }
         }
-
     },
     yAxis: {
         type: 'value',
@@ -177,13 +291,58 @@ option = {
             lineStyle: {
                 color: '#5dc2fe'
             }
-        }
+        },
+        min: 0,
+        max: 800,
+        interval: 100
     },
-    series: [{
-        data: [820, 932, 901, 934, 1290],
-        type: 'line',
-    }],
- 
+    series: [
+        {
+            name: '网络销售量',
+            type: 'line',
+            stack: '总量',
+            data: [120, 132, 101, 134, 90, 230, 100],
+            itemStyle : {
+                normal : {
+                    color:'red',
+                    lineStyle:{
+                        color:'red'
+                    }
+                }
+            }
+        },
+        {
+            name: '实体销售量',
+            type: 'line',
+            stack: '总量',
+            data: [220, 182, 191, 234, 290, 330, 310],
+            itemStyle: {
+                normal: {
+                    color: 'orange',
+                    lineStyle: {
+                        color: 'orange'
+                    }
+                }
+            }
+        },
+        {
+            name: '总销售量',
+            type: 'line',
+            stack: '总量',
+            data: [340, 314, 292, 368, 380, 560, 410],
+            itemStyle : {
+                normal : {
+                    color:'#00FF00',
+                    lineStyle:{
+                        color:'#00FF00'
+                    }
+                }
+            }
+        },
+    ]
 };
 
-outChart.setOption(option);
+lastFiveChart.setOption(option);
+
+
+
